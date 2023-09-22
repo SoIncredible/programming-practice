b391c175a311586d01a95c8812b5416fe650a7d8
李一诺 <liyinuo@dailyinnovation.biz>
2023-09-22 17:45:08 +0800

    使用Terminal编写C#脚本，实现通过Console读取数组的功能

---
+++ Code/2023-9-22-sort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("input numbers:");
+string? input = Console.ReadLine();
+if(input == null){
+	Console.WriteLine("input string is null!");
+	return;
+}
+Regex regex = new Regex(@"\d+");
+MatchCollection matches = regex.Matches(input);
+List<int> numbers = new List<int>();
+foreach(Match match in matches){
+	int number;
+	if(int.TryParse(match.Value, out number)){
+		numbers.Add(number);
+	}
+}
+
+Console.WriteLine("input numbers:");
+foreach(int number in numbers){
+	Console.Write(number + " ");
+}
+Console.WriteLine();
+Console.Write("Hello");
