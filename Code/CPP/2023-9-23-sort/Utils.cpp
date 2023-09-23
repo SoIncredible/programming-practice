7c66a3846cd922254c799c67e7a7472c9d356a1a
EddieLee <263060652@qq.com>
2023-09-23 15:30:41 +0800

    删除头文件中的include

--- Code/CPP/2023-9-23-sort/Utils.cpp
+++ Code/CPP/2023-9-23-sort/Utils.cpp
@@ -1,5 +1,4 @@
 #include"head.h"
-using namespace std;
 void Swap(int &x, int &y){
 	int temp = x;	
 	x = y;
--- Code/CPP/2023-9-23-sort/head.h
+++ Code/CPP/2023-9-23-sort/head.h
@@ -1,5 +1,4 @@
 #ifndef HEAD_H
 #define HEAD_H
-#include<iostream>
 	void Swap(int &x, int &y);
 #endif
