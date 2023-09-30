668da4e2f0ce799920d0f5c4b03481eff9804842
李一诺 <2636060652@qq.com>
2023-09-30 09:00:05 +0800

    已经可以很快的在MacOS上写出DLL了！“

---
+++ Code/CPP/Main.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+#include<dlfcn.h>
+
+using namespace std;
+
+typedef void (*BubbleSort)(int *, int);
+typedef void (*PrintArr)(int *, int);
+
+int main(){
+	
+	void *handle = dlopen("Utils.dylib",RTLD_LAZY);
+
+	if(handle == nullptr){
+		cout << "Load DLL Fail!" << endl;
+		return 1;
+	}
+
+
+	BubbleSort bubbleSort = reinterpret_cast<BubbleSort>(dlsym(handle,"BubbleSort"));
+	PrintArr printArr = reinterpret_cast<PrintArr>(dlsym(handle,"PrintArr"));
+
+	if(bubbleSort == nullptr){
+		cout << "Load BubbleSort Func Fail!" << endl;
+		return 1;
+	}
+
+	if(printArr == nullptr){
+		cout << "Load PrintArr Func Fail!" << endl;
+		return 1;
+	}
+
+
+ 	
+	cout << "Input Random Numbers:(Press X and then Press Enter to end Input)" << endl;
+	
+	int number = 0;
+	vector<int> randomNums;
+
+	while(cin >> number){
+		randomNums.push_back(number);
+	}
+	
+	int arr[randomNums.size()];
+	int size = randomNums.size();
+	
+	for(int i=0; i < size; i++){
+		arr[i] = randomNums[i];
+	}
+
+
+	bubbleSort(arr,size);
+
+	printArr(arr,size);
+
+	dlclose(handle);
+	
+	return 0;
+}
