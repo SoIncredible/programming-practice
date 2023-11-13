9ad89fbda37b9fcb7a1298698181afbe1fecccac
李一诺 <2636060652@qq.com>
2023-11-13 15:44:47 +0800

    MergeSort的编写中遇到了困难

---
+++ Code/CPP/2023-11-8-MergeSort/Head.h
@@ -0,0 +1,7 @@
+#ifndef HEAD_H
+#define HEAD_H
+	int *Merge(int *arr1, int size1, int *arr2, int size2);
+	int *InnerMergeSort(int *arr, int *mergeArr, int size);
+	void MergeSort(int *arr, int size);
+	void PrintArr(int *arr, int size);
+#endif
