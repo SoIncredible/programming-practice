17cb3ba1463bc6144cf7000877b5de9c87c6114e
李一诺 <2636060652@qq.com>
2023-11-16 12:15:41 +0800

    完成2023-11-16 CPP版本QuickSort

---
+++ Code/CPP/2023-11-14-QuickSort/Head.h
@@ -0,0 +1,7 @@
+#ifndef HEAD_H
+#define HEAD_H
+	void PrintArr(int* arr, int size);
+	void QuickSort(int* arr, int size);
+	void InnerQuickSort(int *arr, int begin, int end);
+	void Swap(int &x, int &y);
+#endif
