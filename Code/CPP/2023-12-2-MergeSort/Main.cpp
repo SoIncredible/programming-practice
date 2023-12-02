842bbb65cf24d17cd01a68073e3b9de6ee829b9f
李一诺 <2636060652@qq.com>
2023-12-02 19:08:14 +0800

    完成2023-12-2 CPP版本自底向上的的MergeSort

---
+++ Code/CPP/2023-12-2-MergeSort/Main.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include"Head.h"
+#include<vector>
+using namespace std;
+
+int main(){
+	
+	cout << "Input Unsorted Array" << endl;
+	vector<int> unsortedArr;
+	int number;
+	while(cin >> number){
+		unsortedArr.push_back(number);
+	}
+	
+	int size = unsortedArr.size();
+	int* arr = new int [size];
+	
+	for(int i = 0; i < size; i++){
+		arr[i] = unsortedArr[i];
+	}
+
+	MergeSort(arr, size);
+	PrintArr(arr, size);
+
+	return 0;
+}
+
+void PrintArr(int* arr, int size){
+	cout << "Sorted Array:" << endl;
+	for(int i = 0; i < size; i++){
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+
+}
+
+void MergeSort(int* arr, int size){
+	for(int i = 1; i < size; i*=2){
+		for(int lo = 0; lo < size - i; lo += i + i){
+			Merge(arr, lo, lo + i - 1, (lo + i + i - 1  < size - 1)? lo + i + i - 1 : size - 1, size); 
+		} 
+	}
+}
+
+void Merge(int* arr, int begin, int mid, int end, int size){
+	int* temp = new int[size];
+	int index;
+	for(index = begin; index <= end; index++){
+		temp[index] = arr[index];
+	}
+
+	int left;
+	int right;
+	for(index = begin, left = begin, right = mid + 1; index <= end && left <= mid && right <= end; index++){
+		if(temp[left] < temp[right]){
+			arr[index] = temp[left++];
+		}else{
+			arr[index] = temp[right++];
+		}
+	}
+	while(left <= mid) arr[index++] = temp[left++];
+	while(right <= end)arr[index++] = temp[right++];
+	
+	delete[] temp;
+}
+
