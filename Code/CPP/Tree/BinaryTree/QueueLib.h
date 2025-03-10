63fddbe3def6afdb628902190e8c0ff83c2a21a2
EddieLee <2636060652@qq.com>
2025-03-11 00:48:39 +0800

    完成队列相关逻辑

--- Code/CPP/Tree/BinaryTree/QueueLib.h
+++ Code/CPP/Tree/BinaryTree/QueueLib.h
@@ -1,14 +1,87 @@
+#include <iostream>
+
 template <typename T>
-class Queue{
+struct Node{
+    T *data;
+    Node *next;
+};
+
+template <typename T>
+class Queue
+{
+private:
+    Node<T> *_head;
+    Node<T> *_trail;
+    int _count;
 
 public:
-    bool EnQueue();
+    Queue();
 
-    bool DeQueue();
+    ~Queue();
+
+    void EnQueue(T &data);
+
+    T* DeQueue();
 
     bool IsEmpty();
 
     int Count();
-
-
 };
+
+template <typename T>
+Queue<T>::Queue(){
+    _head = nullptr;
+    _trail = nullptr;
+    _count = 0;
+};
+
+template <typename T>
+Queue<T>::~Queue(){
+    while(_count > 0){
+        Node<T> *h = _head;
+        _head = _head->next;
+        delete h;
+        _count--;
+    }
+}
+
+template <typename T>
+void Queue<T>::EnQueue(T &data){
+    Node<T> *node = new Node<T>();
+    node->data = &data;
+    node->next = nullptr;
+    if(_count == 0){
+        _head = node;
+        _trail = node;
+    }
+    else
+    {
+        _trail->next = node;
+        _trail = node;
+    }
+    _count++;
+}
+
+template <typename T>
+T *Queue<T>::DeQueue() {
+    if(_count == 0){
+        throw std::range_error("队列为空,不能执行出队操作");
+    }
+
+    Node<T> *headNext = _head->next;
+    T *data = _head->data;
+    delete _head;
+    _head = headNext;
+    _count--;
+    return data;
+}
+
+template <typename T>
+bool Queue<T>::IsEmpty(){
+    return _count == 0;
+}
+
+template <typename T>
+int Queue<T>::Count(){
+    return _count;
+}
--- Code/CPP/Tree/BinaryTree/TestStack.cpp
+++ Code/CPP/Tree/BinaryTree/TestStack.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "StackLib.h"
+// #include "StackLib.h"
+#include "QueueLib.h"
 
 class custom{
 public:
@@ -19,26 +20,37 @@ int main(){
     custom c2 = custom(2);
     custom c3 = custom(3);
 
-    Stack<custom> *s = new Stack<custom>();
+    // Stack<custom> *s = new Stack<custom>();
+    Queue<custom> *q = new Queue<custom>();
 
-    s->Push(c1);
-    s->Push(c2);
-    s->Push(c3);
+    c1.data = 222;
+    // s->Push(c1);
+    // s->Push(c2);
+    // s->Push(c3);
+
+    q->EnQueue(c1);
+    q->EnQueue(c2);
+    q->EnQueue(c3);
 
     custom *peek;
-    peek = s->Peek();
-
-    peek->data = 666;
-    std::cout << peek->data << std ::endl;
-    while (!s->IsEmpty())
-    {
-        int count = s->Count();
-        std::cout << "数量" << count << std ::endl;
-        peek = s->Pop();
-        std ::cout << peek->data << std ::endl;
+    while(!q->IsEmpty()){
+        peek = q->DeQueue();
+        std::cout << peek->data << std::endl;
     }
+    // peek = s->Peek();
 
-    delete s;
+    // peek->data = 666;
+    // std::cout << peek->data << std ::endl;
+    // while (!s->IsEmpty())
+    // {
+    //     int count = s->Count();
+    //     std::cout << "数量" << count << std ::endl;
+    //     peek = s->Pop();
+    //     std ::cout << peek->data << std ::endl;
+    // }
+
+    // delete s;
+    delete q;
 
     return 0;
 }
